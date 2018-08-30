ed7526708f9ec5614a317dfddefe78239a3df875
FlatIron <flatiron@Lisas-MacBook-Air.local>
2018-08-30 18:10:19 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_reader :roster
+
+
+
+  def initialize(name)
+    @name = name
+    @roster = Hash.new
+  end
+
+  def add_student(student_name, grade)
+    @add_student = {grade => student_name}
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @grade = @roster[grade]
+  end
+
+  def sort
+    @roster[grade].sort
+  end
+
+end
