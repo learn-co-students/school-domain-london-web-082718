0ffc82ba0ee5a17bcd947f856b34bc520dd95eab
Billie Simmons <billiesnyc@gmail.com>
2018-08-30 11:22:52 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,40 @@
-# code here!
+require 'pry'
+
+class School
+  
+  attr_reader :roster
+  
+  def initialize(school_name)
+    @school_name = school_name 
+    @roster = {}
+  end
+  
+  
+  def add_student(student_name, student_grade)
+    @student_name = student_name
+    @student_grade = student_grade
+    if @roster[@student_grade]
+      @roster[@student_grade] << student_name
+    else 
+    @roster[@student_grade] = [ ]
+    @roster[@student_grade] << student_name
+  end
+  end
+  
+  def grade(grade)
+    @grade = grade
+    holding_hash = { }
+    holding_hash = @roster.select do |grades, students|
+    grades == @grade
+    end
+    holding_hash.values.flatten
+  end
+  
+  def sort
+    @roster.each do |grades, students|
+      @roster[grades] = students.sort
+     
+    end
+  end
+  
+end
