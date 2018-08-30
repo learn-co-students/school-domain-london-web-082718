6d2b40ce712c6235a869c98370770375d91da878
thexyman <makdaa1@gmail.com>
2018-08-30 13:40:40 +0100

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,42 @@
+require 'pry'
+
 # code here!
+class School
+
+  attr_accessor :name
+  attr_reader :roster
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade]
+      @roster[grade] << name
+    else
+    @roster[grade] = []
+    @roster[grade] << name
+  end
+  end
+
+  def grade(grade)
+    students = []
+    @roster.map do |key, value|
+      if key == grade
+        students << value
+      else
+        puts "no students found"
+      end
+    end
+    students.flatten
+  end
+
+  def sort
+    @roster.each do |grades, students|
+      @roster[grades] = students.sort
+    end
+  end
+
+
+end
